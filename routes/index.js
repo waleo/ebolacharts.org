58d8ef4e9b869a5b379d225067c3c22e06ed6a46
waleo <writewale@gmail.com>
2014-11-08 20:34:58 -0500

    basic seo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ebola 2014 Interactive Chart Data Visualization',
+                        description: 'Interactive Chart and Data for the 2014 Ebola Outbreak',
+                        keywords: 'ebola, 2014, ebola 2014, chart, data, ebola charts, ebola data, outbreak, virus, graph',
+                        author: 'Akinwale Wale Olaleye'});
 });
 
 module.exports = router;
